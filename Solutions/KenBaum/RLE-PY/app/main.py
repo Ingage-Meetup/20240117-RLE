5e4d415e66d162f610a164c3b41fa4887d72b985
Ken Baum <ilpadre1953@gmail.com>
2024-01-15 16:23:59 -0500

    added code

--- Solutions/KenBaum/RLE-PY/app/main.py
+++ Solutions/KenBaum/RLE-PY/app/main.py
@@ -1,5 +1,21 @@
+import utils
+
+
+
 def main():
     print("Hello, World!")
+    x = utils.add(1,3)
+    print(x)
+    import os
+    print("Current Working Directory: ", os.getcwd())
+
+    encoding = utils.rle(utils.readFile('app/files/iliad.txt'))
+    utils.writeFile("app/files/iliad.encoding", encoding)
+
+    # bmp = utils.load_bitmap('app/files/sample.bmp')
+    # encoded_list = utils.run_length_encode(bmp)
+    # encoding = ' '.join(str(item) for item in encoded_list)
+    # utils.writeFile("app/files/samplebmp.encoding", encoding)
 
 
 if __name__ == "__main__":
--- Solutions/KenBaum/RLE-PY/app/utils.py
+++ Solutions/KenBaum/RLE-PY/app/utils.py
@@ -1,6 +1,83 @@
+from PIL import Image
+import numpy as np
+
 def add(a, b):
     return a + b
 
-
+\
 def subtract(a, b):
     return a - b
+
+def readFile(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            file_contents = file.read()
+            return file_contents
+    except UnicodeDecodeError:
+        with open(file_path, 'r', encoding='cp1252') as file:
+            file_contents = file.read()
+        return file_contents
+    
+def readBinaryFile(file_path):
+
+    with open(file_path, 'rb') as file:
+        binary_data = file.read()
+        try:
+            text_data = binary_data.decode('utf-8')
+            return text_data
+        except UnicodeDecodeError: 
+            print("Error")
+            return "" 
+    
+def writeFile(file_path, contents):
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(contents)
+
+def rle(original_file_contents):
+    if not original_file_contents:
+        return "[]"
+    prevChar = original_file_contents[0]
+    charCount = 1
+    encodedString = "["
+    for char in original_file_contents[1:]:
+        if char != prevChar:
+            encodedString += "(" + str(prevChar) + "," + str(charCount) + "),"
+            prevChar = char
+            charCount=1
+        else:
+            charCount+=1
+    encodedString += "(" + str(prevChar) + "," + str(charCount) + ")]"
+    return encodedString
+
+
+def run_length_encode(img):
+    """
+    Apply simple run-length encoding to an image.
+    """
+    data = np.array(img)
+    flat_data = data.flatten()
+    encoded = []
+    
+    prev_pixel = flat_data[0]
+    count = 1
+    
+    for pixel in flat_data[1:]:
+        if pixel == prev_pixel:
+            count += 1
+        else:
+            encoded.append((prev_pixel, count))
+            prev_pixel = pixel
+            count = 1
+
+    # Add the last run
+    encoded.append((prev_pixel, count))
+    return encoded
+
+def load_bitmap(file_path):
+    """
+    Load a bitmap image and convert it to grayscale.
+    """
+    img = Image.open(file_path).convert('L')  # Convert to grayscale
+    return img
+
+
